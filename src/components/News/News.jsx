789f2765183e5515708439a0b14cf24302c2a5b4
Danya <daniilerkesov122@gmail.com>
2024-05-02 09:40:10 +0300

    Create News-component

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 
 import { fetchNewsData } from "../../redux/slice/News";
 import { selectNewsItem, selectIsLoading } from "../../redux/slice/News";
+import { Section } from "../Section/Section";
 
 export const News = () => {
     const dispatch = useDispatch();
@@ -10,11 +11,23 @@ export const News = () => {
     const newsItem = useSelector(selectNewsItem);
     const isLoading = useSelector(selectIsLoading);
 
-    const filteredINewsItems = Array.from(newsItem);
+    const filteredINewsItems = Array.from(newsItem).filter((_, i) => i < 3);
 
     useEffect(() => {
         dispatch(fetchNewsData());
     }, [dispatch]);
 
-    return <div>News</div>;
+    return (
+        <div className="news-container">
+            <Section>
+                <div className="container">
+                    <h2>Новости</h2>
+                    <div className="news">
+                        <img src="" alt="" />
+                        <p className="news-text"></p>
+                    </div>
+                </div>
+            </Section>
+        </div>
+    );
 };
